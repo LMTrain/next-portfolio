15be30f2484accffb83531fb64f5fb8bd5d06c72
LMTrain <layconmm@msn.com>
2020-08-28 00:08:14 -0500

    Add global jsx

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,20 @@
 import Link from 'next/Link';
 import Layout from '../components/Layout';
+import { Component } from 'react';
 
-export default () => (
-    <Layout title="About">
+export default class About extends Component {
+    render() {
+        return (
+            <Layout title="About">
        
-        <Link href="/">
-            <a>Go to home</a>
-        </Link>
-        <p>A JavaScript programmer</p>
-        <img src="/static/logo512.png" alt="React logo" height="200px"/>
-    </Layout>
-);
+                <Link href="/">
+                    <a>Go to home</a>
+                </Link>
+                <p>A JavaScript programmer</p>
+                <img src="/static/logo512.png" alt="React logo" height="200px"/>
+            </Layout>
+        );
+
+    }
+}
+    
