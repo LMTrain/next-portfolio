b91746d251478fcf8e0b1747c77bafd7ea6c08e8
LMTrain <layconmm@msn.com>
2020-08-29 20:17:25 -0500

    Fetching Data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,42 @@
 import Link from 'next/Link';
 import Layout from '../components/Layout';
+// import fetch from "isomorphic-unfetch";
 import { Component } from 'react';
 
 export default class About extends Component {
+    // state = {
+    //     user: null
+    // };
+    static async getInitialProps() {
+            const res = await fetch('https://api.github.com/users/lmtrain')
+            const data = await res.json();
+            
+            return { user: data};
+    }
+    // componentDidMount() {
+    //     fetch('https://api.github.com/users/lmtrain') npm i isomorphic-unfetch
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             this.setState({
+    //                 user: data
+    //             });
+    //         })
+    // }
     render() {
+        const { user } = this.props;
         return (
             <Layout title="About">
-       
-                <Link href="/">
-                    <a>Go to home</a>
-                </Link>
-                <p>A JavaScript programmer</p>
-                <img src="https://lmtrain.github.io/lm-images/assets/images/bugatti_1.jpg" alt="React logo" height="200px"/>
+                
+                <p>{user.name}</p>
+                <img src={user.avatar_url} alt="LMTrain Pic" height="200px"/>
                 <img src="https://lmtrain.github.io/lm-images/assets/images/bugatti_5.jpg" alt="React logo" height="200px"/>
             </Layout>
         );
 
     }
 }
+
+// About.getInitialProps = () => {
+
+// }
     
