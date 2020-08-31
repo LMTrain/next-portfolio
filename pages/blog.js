9244c841df71d7b493e9d0eda442ae048d80d7d5
LMTrain <layconmm@msn.com>
2020-08-30 23:27:21 -0500

    Cleaner Urls witout Query Params

--- pages/blog.js
+++ pages/blog.js
@@ -2,9 +2,9 @@ import Layout from '../components/Layout';
 import Link from 'next/link';
 
 
-const PostLink = ({ title }) => (
+const PostLink = ({ slug, title }) => (
     <li>
-        <Link href={`/post?title=${title}`}>
+        <Link as={`/${slug}`} href={`/post?title=${title}`}>
             <a>{title}</a>
         </Link>
     </li>
@@ -13,10 +13,10 @@ const PostLink = ({ title }) => (
 export default () => (
     <Layout title="My Blog">
         <ul>
-            <PostLink title= "react" />
-            <PostLink title= "angular" />
-            <PostLink title= "vue" />
-            <PostLink title= "nextjs" />
+            <PostLink slug="react-post" title= "React Post" />
+            <PostLink slug="angular-post" title= "Angular Post" />
+            <PostLink slug="vue-post" title= "Vue Post" />
+            <PostLink slug="nextjs-post" title= "Nextjs Post" />
         </ul>
     </Layout>
 )
--- pages/post.js
+++ pages/post.js
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout';
+import { withRouter } from 'next/router';
 
-const Post = ({ url }) => (
-    <Layout title={url.query.title}>
+
+const Post = ({ router }) => (
+    <Layout title={router.query.title}>
         <p>Lorem kjahak jajh ja na vah hfna vna va nbv hgbafhgvsadg hgdfnu fhuyga ufajag fayjavna jkhahvah aac jycf
             ghsgak nhgayua jgagjhhasgc ma hvag ahf chsa ahfch jhgahsfch sdy jhsgj agc hah sn hgvdsfacuyw zk ewhganv j   hafchfn nvsnfa
              hsgvhs has sah h vhgc khas ngus c gcjgjsgcu.
@@ -9,4 +11,4 @@ const Post = ({ url }) => (
     </Layout>
 );
 
-export default Post;
+export default withRouter(Post);
