658e1d4f0cfee8b6065c4ed48dd2fed2c34bc40e
LMTrain <layconmm@msn.com>
2020-09-05 17:56:16 -0500

    Adding Comments to Story

---
+++ HACKER-NEXT/components/CommentList.js
@@ -0,0 +1,8 @@
+import Comment from './Comment'
+export default ({ comments }) => (
+    <React.Fragment>
+        {comments.map(comment => (
+            <comment key={comment.id} comment={comment} />
+        ))}
+    </React.Fragment>
+)
