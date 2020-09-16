03fe96d3070f7bc6501c22064fe86b5064d92556
LMTrain <layconmm@msn.com>
2020-09-16 13:36:50 -0500

    Add LoginForm in NEXT-COOKIE-AUTH

---
+++ NEXT-COOKIE-AUTH/components/LoginForm.js
@@ -0,0 +1,43 @@
+import { loginUser } from '../lib/auth';
+
+
+class LoginForm extends React.Component {
+    state = {
+        email: '',
+        password: ''
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = event => {
+        const { email, password } = this.state;
+
+        event.preventDefault();
+        loginUser(email, password);
+        console.log(this.state);
+        
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div><input type="email"
+                    name="email"
+                    placeholder="email"
+                    onChange={this.handleChange}
+                /></div>
+                <div><input type="pasword"
+                    name="password"
+                    placeholder="password"
+                    onChange={this.handleChange}
+                /></div>
+                <button type="submit">Submit</button>
+            </form>
+
+        )
+    }
+}
+
+export default LoginForm;
