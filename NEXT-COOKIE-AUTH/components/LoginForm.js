b5a79540e1d287b2ccb7182916fcf37d060ec2a2
LMTrain <layconmm@msn.com>
2020-09-23 21:47:03 -0500

    Add profile.js page  page in NEXT-COOKIE-AUTH

--- NEXT-COOKIE-AUTH/components/LoginForm.js
+++ NEXT-COOKIE-AUTH/components/LoginForm.js
@@ -5,7 +5,9 @@ import Router from 'next/router';
 class LoginForm extends React.Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: '',
+        isLoading: false
     };
 
     handleChange = event => {
@@ -16,14 +18,22 @@ class LoginForm extends React.Component {
         const { email, password } = this.state;
 
         event.preventDefault();
+        this.setState({ error: '', isLoading: true });
         loginUser(email, password).then(() =>{
             Router.push("/profile");
-        });
-        console.log(this.state);
-        
+        })
+        .catch(this.showError);
+        // console.log(this.state);
     };
 
+    showError = err => {
+        console.error(err);
+        const error = err.response && err.response.data || err.message;
+        this.setState({ error, isLoading: false });
+    }
+
     render() {
+        const { error, isLoading } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
                 <div><input type="email"
@@ -36,8 +46,10 @@ class LoginForm extends React.Component {
                     placeholder="password"
                     onChange={this.handleChange}
                 /></div>
-                <button type="submit">Submit</button>
-                <button type="submit">Submit</button>
+                <button disabled={isLoading} type="submit">
+                    {isLoading ? "Sending" : "Submit"}
+                </button>
+                {error && <div>{error}</div>}
             </form>
 
         )
