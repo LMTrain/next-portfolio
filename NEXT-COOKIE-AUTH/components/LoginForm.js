f921ff8ae28615bbe2a295752888c219f301eebc
LMTrain <layconmm@msn.com>
2020-10-14 21:32:12 -0500

    Add more codes in LoginForm.js in NEXT-COOKIE-AUTh

--- NEXT-COOKIE-AUTH/components/LoginForm.js
+++ NEXT-COOKIE-AUTH/components/LoginForm.js
@@ -1,63 +1,67 @@
-import { loginUser } from '../lib/auth';
-import Router from 'next/router';
-
+import Router from "next/router";
+import { loginUser } from "../lib/auth";
 
 class LoginForm extends React.Component {
-    state = {
-        email: 'Rey.Padberg@karina.biz',
-        password: 'ambrose.net',
-        error: '',
-        isLoading: false
-    };
+  state = {
+    email: "Lucio_Hettinger@annie.ca",
+    password: "demarco.info",
+    isLoading: false,
+    error: ""
+  };
 
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    handleSubmit = event => {
-        const { email, password } = this.state;
+  handleSubmit = event => {
+    const { email, password } = this.state;
 
-        event.preventDefault();
-        this.setState({ error: '', isLoading: true });
-        loginUser(email, password).then(() =>{
-            Router.push("/profile");
-        })
-        .catch(this.showError);
-        // console.log(this.state);
-    };
+    event.preventDefault();
+    this.setState({ error: "", isLoading: true });
+    loginUser({ email, password })
+      .then(() => {
+        Router.push("/profile");
+        this.setState({ isLoading: false });
+      })
+      .catch(this.setError);
+  };
 
-    showError = err => {
-        console.error(err);
-        const error = err.response && err.response.data || err.message;
-        this.setState({ error, isLoading: false });
-    }
+  setError = err => {
+    console.error(err);
+    const error = (err.response && err.response.data) || err.message;
+    this.setState({ error, isLoading: false });
+  };
 
-    render() {
-        const { email, password, error, isLoading } = this.state;
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div><input 
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    value={email}
-                    onChange={this.handleChange}
-                /></div>
-                <div><input type="pasword"
-                    name="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={this.handleChange}
-                />
-                </div>
-                <button disabled={isLoading} type="submit">
-                    {isLoading ? "Sending" : "Submit"}
-                </button>
-                {error && <div>{error}</div>}
-            </form>
+  render() {
+    const { email, password, error, isLoading } = this.state;
 
-        );
-    }
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Sending" : "Submit"}
+        </button>
+        {error && <div>{error}</div>}
+      </form>
+    );
+  }
 }
 
 export default LoginForm;
