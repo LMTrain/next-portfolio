c971712cab49f746ee090e8b997ac7f298d1ff14
LMTrain <layconmm@msn.com>
2020-10-16 17:18:25 -0500

    Add more codes in _document.js in NEXT-COOKIE-AUTh

--- NEXT-COOKIE-AUTH/pages/_document.js
+++ NEXT-COOKIE-AUTH/pages/_document.js
@@ -1,27 +1,26 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { getServerSideToken, getUserScript } from "../lib/auth";
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const props = await Document.getInitialProps(ctx);
-        const userData = await getServerSideToken(ctx.req);
+  static async getInitialProps(ctx) {
+    const props = await Document.getInitialProps(ctx);
+    /* include, otherwise we will get an error telling us: Warning: Expected server HTML to contain a matching <div> in <div>. */
+    const authData = getServerSideToken(ctx.req);
+    return { ...props, ...authData };
+  }
 
-        return { ...props, ...userData }
-    }
+  render() {
+    const { user = {} } = this.props;
 
-    render() {
-        const { user = {} } = this.props;
-
-        return (
-            <html>
-                <Head />
-                <body>
-                    <Main />
-                    <script dangerouslySetInnerHTML={{ __html: getUserScript(user) }}
-                    />
-                    <NextScript />
-                </body>
-            </html>
-        )
-    }
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <script dangerouslySetInnerHTML={{ __html: getUserScript(user) }} />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
