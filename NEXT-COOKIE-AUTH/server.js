0806853f8a23982a3165329c193b813b9a1023fd
LMTrain <layconmm@msn.com>
2020-10-19 21:57:18 -0500

    Add more codes in server.js in NEXT-COOKIE-AUTh

--- NEXT-COOKIE-AUTH/server.js
+++ NEXT-COOKIE-AUTH/server.js
@@ -1,72 +1,80 @@
-const next = require('next');
-const express = require('express');
-const axios = require('axios');
-const cookieParser = require('cookie-parser');
+const next = require("next");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
+
+const AUTH_USER_TYPE = "authenticated";
+const COOKIE_SECRET = "secret123";
+const COOKIE_OPTIONS = {
+  // domain: 'YOUR_DOMAIN',
+  // path: "/",
+  secure: !dev,
+  httpOnly: true,
+  signed: true
+};
+
+const authenticate = async (email, password) => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return data.find(user => {
+    if (user.email === email && user.website === password) {
+      return user;
+    }
+  });
+};
+
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const AUTH_USER_TYPE = "authenticated";
-const COOKIE_SECRET = 'Lily13362';
-const COOKIE_OPTIONS = {
-    httpOnly: true,
-    secure: !dev,
-    signed: true
-};
-
-const authenticate = async (email, password) => {
-    const { data } = await axios.get
-    ('https://jsonplaceholder.typicode.com/users');
-    return data.find(user => {
-        if (user.email === email && user.website === password) {
-            return user;
-        }
-    })
-}
-
 app.prepare().then(() => {
-    const server = express();
+  const server = express();
 
-    server.use(express.json());
-    server.use(cookieParser(COOKIE_SECRET))
+  server.use(express.json());
+  server.use(cookieParser(COOKIE_SECRET));
 
-    server.post('/api/login', async (req, res) => {
-        const { email, password } = req.body;
-        const user = await authenticate(email, password);
-        if (!user) {
-            return res.status(403).send("Invalid email or Password");
-        }
-        const userData = {
-            name: user.name,
-            email: user.email,
-            type: AUTH_USER_TYPE
-        }
-        res.cookie('token', userData, COOKIE_OPTIONS);
-        res.json(userData);
-    });
+  server.post("/api/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await authenticate(email, password);
+    if (!user) {
+      return res.status(403).send("Invalid email or password");
+    }
+    const userData = {
+      name: user.name,
+      email: user.email,
+      type: AUTH_USER_TYPE
+    };
+    res.cookie("token", userData, COOKIE_OPTIONS);
+    res.json(userData);
+  });
 
-    server.get('/api/profile', async (req, res) => {
-        const { signedCookies = {} } = req;
-        const { token } = signedCookies;
-        if (token && token.email) {
-            const { data } = await axios.get
-                ('https://jsonplaceholder.typicode.com/users'
-            );
-            const userProfile = data.find(user => user.email === token.email);
-            return res.json({ user: userProfile });
-        }
-        res.sendStatus(404);
-    })
+  server.post("/api/logout", (req, res) => {
+    res.clearCookie("token", COOKIE_OPTIONS);
+    res.sendStatus(204);
+  });
 
-    server.get("*", (req, res) => {
-        return handle(req, res);
-    });
+  server.get("/api/profile", async (req, res) => {
+    const { signedCookies = {} } = req;
+    const { token } = signedCookies;
+    if (token && token.email) {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const userProfile = data.find(user => user.email === token.email);
+      return res.json({ user: userProfile });
+    }
+    res.sendStatus(404);
+  });
 
-    server.listen(port, err => {
-        if (err) throw err;
-        console.log(`Listening on PORT ${port}`);
-    });
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
 
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`Listening on PORT ${port}`);
+  });
 });
