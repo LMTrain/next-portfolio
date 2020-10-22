88386c20c16393bbc7a74729fbf071b492783954
LMTrain <layconmm@msn.com>
2020-10-21 22:26:57 -0500

    Add more codes in login.js in NEXT-COOKIE-AUTh

--- NEXT-COOKIE-AUTH/pages/login.js
+++ NEXT-COOKIE-AUTH/pages/login.js
@@ -1,10 +1,13 @@
-import Layout from '../components/Layout';
-import LoginForm from '../components/LoginForm';
+import Layout from "../components/Layout";
+import LoginForm from "../components/LoginForm";
+import { authInitialProps } from "../lib/auth";
 
-export default function Login() {
-    return (
-        <Layout title="Login">
-            <LoginForm />
-        </Layout>
-    );
+export default function Login(props) {
+  return (
+    <Layout title="Login" {...props}>
+      <LoginForm />
+    </Layout>
+  );
 }
+
+Login.getInitialProps = authInitialProps();
