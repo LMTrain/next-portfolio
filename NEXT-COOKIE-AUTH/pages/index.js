2e0c5a8d06ac8fbf5c644f3c4661aec9ddbe6934
LMTrain <layconmm@msn.com>
2021-01-25 22:10:35 -0600

    Add comment to pages/index.js in LU-CONNECT

--- NEXT-COOKIE-AUTH/pages/index.js
+++ NEXT-COOKIE-AUTH/pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import { authInitialProps } from "../lib/auth";
 
+//Create Function for Home with props
 export default function Home(props) {
   return (
     <Layout title="Home" {...props}>
