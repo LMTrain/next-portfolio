88c732a81f12ec56e44c6e0982d3d7f4c24af26a
LMTrain <layconmm@msn.com>
2021-02-18 21:08:14 -0600

    Correct spelling mistake

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 const Index = () => (
     <Layout title="Home">
-        <p>Welcome to the ome page</p>
+        <p>Welcome to the Home page</p>
     </Layout>
 );
 
